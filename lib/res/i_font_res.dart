5b9729bd0e03273d0c3382e360d34ae611ccd1dc
Harbey <anabassem5@gmail.com>
2022-03-13 19:09:11 +0200

    first commit

---
+++ lib/res/i_font_res.dart
@@ -0,0 +1,8 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class FontRes {
+  static const String ANTON_REGULAR = 'Anton-Regular';
+  static const String LATO_REGULAR = 'Lato-Regular';
+}
